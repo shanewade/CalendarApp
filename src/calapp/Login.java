ff014c1f10f1e0e0eadff9a876924812c659bf2a
Shane Wade <shanewade60@gmail.com>
2017-02-07 22:05:46 -0600

    More Login Validation
    Added in a visual to the screen when login works.  Removed the
    localization test file and implemented features in the main login page.

--- src/calapp/Login.java
+++ src/calapp/Login.java
@@ -1,5 +1,9 @@
 package calapp;
 
+import java.util.Locale;
+import javafx.scene.control.Dialog;
+import java.util.ResourceBundle;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -10,13 +14,16 @@ import javafx.scene.text.*;
 import javafx.stage.*;
 
 public class Login extends Application{
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		
+		Locale locale = Locale.getDefault();
+		//Locale.setDefault(locale);
+		System.out.println(locale);
+		ResourceBundle rb = ResourceBundle.getBundle("ApplicationResources");
 		//Set up the initial grid
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
@@ -26,25 +33,28 @@ public class Login extends Application{
 		
 		//Add the grid to the Scene
 		Scene scene = new Scene(grid, 300, 275);
-		Text scenetitle = new Text("Welcome");
+		Text scenetitle = new Text(rb.getString("greeting"));
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		grid.add(scenetitle, 0, 0, 2, 1);
 		
 		//Setup userName and Password Inputs
-		Label userName = new Label("User Name:");
+		Label userName = new Label(rb.getString("username"));
 		grid.add(userName, 0, 1);
 
 		TextField userTextField = new TextField();
 		grid.add(userTextField, 1, 1);
 
-		Label pw = new Label("Password:");
+		Label pw = new Label(rb.getString("password"));
 		grid.add(pw, 0, 2);
 
 		PasswordField pwBox = new PasswordField();
 		grid.add(pwBox, 1, 2);
 		
+		Label errorText = new Label();
+		grid.add(errorText, 0, 5);
+		
 		//Add the Sign In button
-		Button btn = new Button("Sign in");
+		Button btn = new Button(rb.getString("login"));
 		HBox hbBtn = new HBox(10);
 		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
 		hbBtn.getChildren().add(btn);
@@ -58,12 +68,28 @@ public class Login extends Application{
 		
 		//add action on button press
 		
+
+		
 		btn.setOnAction(e -> {
-            String value1 = String.valueOf(userTextField.getText());
-            String value2 = String.valueOf(pwBox.getText());
-            System.out.println(ValidateLogin.checkPassword(value1, value2));
-            
+			String value1 = String.valueOf(userTextField.getText());
+	        String value2 = String.valueOf(pwBox.getText());
+			Boolean validlogin = ValidateLogin.checkPassword(value1, value2);
+			if (validlogin == true) {
+				errorText.setText("Logged In");
+				System.out.println("You have logged in to the system");
+			}
+			else {
+				errorText.setText("Bad Password");
+				System.out.println("Invalid loging credentials");
+			}
+	        
+	        //System.out.println(ValidateLogin.checkPassword(value1, value2));
+			
 		});
+		
+
+		
+		
 	}
 	
 
