e9a375e1a4fdfbe52f01219ea04875cd6d9e2388
Shane Wade <shanewade60@gmail.com>
2017-02-23 21:45:08 -0600

    Completed Validation of Add New Customer
    Also got switching back to main screen working on successful save and
    cancel.

--- src/calapp/NewCustomerController.java
+++ src/calapp/NewCustomerController.java
@@ -9,7 +9,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -26,7 +30,8 @@ public class NewCustomerController implements Initializable {
     @FXML private TextField custCountry;
     
     @FXML private void saveNewCust(){
-//        
+//       
+        if (validate()) {
         Customer newCust = new Customer (Customer.getNextCustID(), custName.getText(), Address.getCurrentOrGetNextAddressID(custAddress.getText()), City.getCurrentOrGetNextCityID(custCity.getText())
                                          ,Country.getCurrentOrGetNextCountryID(custCountry.getText()), custPhone.getText() );
         
@@ -42,10 +47,78 @@ public class NewCustomerController implements Initializable {
         Address.addNewAddress(custAddress.getText(), custPhone.getText(), City.getCurrentOrGetNextCityID(custCity.getText()), custZip.getText());
         
         Customer.addNewCustomer(newCust.getCustid(newCust), newCust.getCustName(newCust),Address.getCurrentOrGetNextAddressID(custAddress.getText()));
+        
+        custscreen(); 
+        
+            }
+        
+        
+        }
+
+    
+    @FXML private void custscreen(){
+        try {
+            Control c = Control.getInstance();
+            System.err.println();
+            Stage stage = c.getStage();
+            c.SetStage(stage);
+            c.SetPane("custEntry");             
+
+            } catch (Exception e1) {
+                System.err.println(e1);
+            }
     }
-    /**
-     * Initializes the controller class.
-     */
+    
+    private boolean validate() {
+        String name = custName.getText();
+        String address = custAddress.getText();
+        String phone = custPhone.getText();
+        String city = custCity.getText();
+        String zip = custZip.getText();
+        String country = custCountry.getText();
+        
+        if (name.length()<3 ){
+            alert("Name is not long enough.  Min character length is 3");
+            return false;
+        }
+        
+        if (address.length()< 5){
+            alert("Address is not long enough.  Min character length is 5");
+            return false;
+        }
+        if (phone.length()<7){
+            alert("Phone number is not long enough.  Min character length is 7");
+            return false;
+        }
+        if (city.length()<2){
+            alert("City name is not long enough.  Min character length is 2");
+            return false;
+        }
+        if (zip.length()<5){
+            alert("Zip is not long enough.  Min character length is 5");
+            return false;
+        }
+        if (country.length()<2){
+            alert("Country name is not long enough.  Min character length is 2");
+            return false;
+        }
+        
+        return true;
+    }
+    
+    private void alert(String context){
+        Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Save New Customer");
+            alert.setHeaderText("Validation Error");
+            alert.setContentText(context);
+            alert.showAndWait().ifPresent(rs -> {
+                if (rs == ButtonType.OK) {
+                System.err.println(context);
+                }
+                    
+            });
+}
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
